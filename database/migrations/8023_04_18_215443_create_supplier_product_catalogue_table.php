8f3518221e262a046d8146cd493ccf8024303a40
Michael Owen Oduor <michaeloduor50@gmail.com>
2023-04-18 22:03:50 +0300

    Created Supplier Product Catalogue Table

---
+++ database/migrations/8023_04_18_215443_create_supplier_product_catalogue_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('supplier_product_catalogue', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('supplier_id');
+            $table->string('product_code');
+            $table->string('remaining_amount');
+            $table->timestamps();
+            
+            $table->foreign('supplier_id')->references('id')->on('suppliers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('supplier_product_catalogue');
+    }
+};
